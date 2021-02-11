fe7fe52b776cbb225a16605b16c7011d376f11f6
Steve Datz <sfdatz@live.com>
2021-02-11 09:42:00 -0600

    Cleaned up part displays on bin scripts, adjusted front collider for invisible walls

---
+++ Assets/Scripts/UI/UIPartCountDisplayDefect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UIPartCountDisplayDefect : UIPartCountDisplay
+{
+    ///DEFECT- Shows no Object , shows text if not empty 
+
+
+    void LateUpdate()
+    {
+        if(_manager)
+        {
+            if(_manager.IsInitalized)
+            {
+                DisableText(false);
+
+                int current = _manager.SlotsInUse();
+                int max = _manager.MaxSlots();
+
+                ShowIfNotEmpty(current, max);
+
+                return;
+            }
+        }
+
+        DisableText(true);
+    }
+    public void ShowIfNotEmpty(int current, int max)
+    {
+
+        if (current == 0)
+        {
+            DisableText(true);
+
+        }
+        else
+        {
+            UpdateText(current, max); ///Not sure?
+        }
+    }
+    protected override void UpdateText(int current, int max)
+    {
+       _text.text = $"{current}";
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/UI/UIPartCountDisplayIn.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UIPartCountDisplayIn : UIPartCountDisplay
+{
+    ///IN- Shows drop Arrow when Not Empty, shows text if not empty
+
+    [SerializeField] GameObject _dropPartsArrow;
+
+    void LateUpdate()
+    {
+        if (_manager)
+        {
+            if (_manager.IsInitalized)
+            {
+                DisableText(false);
+
+                int current = _manager.SlotsInUse();
+                int max = _manager.MaxSlots();
+
+                ShowIfNotEmpty(current, max);
+                return;
+            }
+        }
+
+        DisableText(true);
+    }
+
+    public void ShowIfNotEmpty(int current, int max)
+    {
+        if (current != 0)
+        {
+            DisableText(true);
+            ///Show Button
+            if (_dropPartsArrow)
+                _dropPartsArrow.SetActive(true);
+
+        }
+        else
+        {
+            UpdateText(current, max); ///Not sure?
+            ///Don't Show Button
+            if (_dropPartsArrow)
+                _dropPartsArrow.SetActive(false);
+
+        }
+    }
+
+
+
+
+
+}
