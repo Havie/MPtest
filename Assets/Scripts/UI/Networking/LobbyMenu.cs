7b4d9454d8a59dee3cc988a2c19d725dc5122754
Steve Datz <sfdatz@live.com>
2021-03-22 11:14:38 -0500

    Menu start

---
+++ Assets/Scripts/UI/Networking/LobbyMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyMenu : MonoBehaviour
+{
+
+    [SerializeField] Transform _lobbyDiv = default;
+
+    private GameObject _lobbyRowPrefab;
+
+
+    private void Awake()
+    {
+        _lobbyRowPrefab = Resources.Load<GameObject>("UI/LobbyRow");
+    }
+
+    public void PlayerConnected()
+    {
+        
+    }
+
+
+    private void CreateRow()
+    {
+        var row = Instantiate(_lobbyRowPrefab, _lobbyDiv);
+    }
+}
---
+++ Assets/Scripts/UI/Networking/LobbyRow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LobbyRow : MonoBehaviour
+{
+    [SerializeField] Text _playerNumber = default;
+    [SerializeField] Text _playerName = default;
+    [SerializeField] Dropdown _stationDropDown = default;
+    [SerializeField] Button _taskInfo = default;
+    [SerializeField] Text _outputLabel = default;
+
+
+    public void initialize(int num, string name, string outputStation)
+    {
+        _playerNumber.text = num.ToString();
+        _playerName.text = name;
+        _outputLabel.text = outputStation;
+    }
+
+}
