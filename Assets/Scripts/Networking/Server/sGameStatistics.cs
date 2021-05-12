5892f58c04be616336b95660ed90b71f938597e1
Steve Datz <sfdatz@live.com>
2021-05-12 10:10:15 -0500

    Verifying statistics, will need to subtract IDLE time for proper cycle time

--- Assets/Scripts/Networking/Server/sGameStatistics.cs
+++ Assets/Scripts/Networking/Server/sGameStatistics.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class sGameStatistics
 {
 
@@ -38,38 +39,21 @@ public class sGameStatistics
     public int GetShippedOnTime() => ShippedOnTime;
     public int GetShippedLate() => ShippedLate;
 
+    /// <summary>
+    /// The avg time it takes to get an item(/batch?) through the simulation to shipping
+    /// </summary>
     public float GetThroughput()
     {
         var totalShipped = GetTotalShipped();
-
+        if(totalShipped>0)
+            UIManager.DebugLog($"result= <color=green> {totalShippingTime} / {totalShipped}  </color>");
+        else
+            UIManager.DebugLog($"result= <color=red> {_currentRoundEndtime} / {_currentRoundTimeStart}  </color>");
         return totalShipped > 0 ? ( totalShippingTime / totalShipped) : (_currentRoundEndtime - _currentRoundTimeStart);
     }
-    public void AddedADefect(int stationID, int itemID) { ++Defects; }
-    public void CreatedAnOrder(int itemID, float createdTime, float expectedTime)
-    {
-        ItemOrder order = new ItemOrder(itemID, createdTime, expectedTime);
-        _orders.Enqueue(order);
-    }
-    public void StationSentBatch(int stationID, int batchSize, bool wasShipped, float time)
-    {
-        Debug.Log($"<color=White> THIS SHIPPPPPED</color> = {wasShipped}  --> ORDERCOUNT= {_orders.Count}");
-        if (wasShipped)
-            ShippedAnOrder(time);
-
-        ///Keep track of cycletime
-        if (_cycleTimes.TryGetValue(stationID, out Queue<float> times))
-        {
-            times.Enqueue(time);
-            return;
-        }
-
-        ///Creates a new Entry
-        Queue<float> newTimesQueue = new Queue<float>();
-        //newTimesQueue.Enqueue(_timeGameStarted); //perhaps not?
-        newTimesQueue.Enqueue(time);
-        _cycleTimes.Add(stationID, newTimesQueue);
-
-    }
+    /// <summary>
+    /// The time it takes a station to move work through (time it takes to do station task)
+    /// </summary>
     public float GetCycleTimeForStation(int stationID)
     {
         bool detailedINFO = false;
@@ -77,7 +61,7 @@ public class sGameStatistics
         if (_cycleTimes.TryGetValue(stationID, out Queue<float> times))
         {
             int cycles = times.Count;
-           
+            //Debug.Log($"{stationID}:: Number of cycles<color=yellow> {cycles} </color>  end:{_currentRoundEndtime} - start:{_currentRoundTimeStart}");
             if (detailedINFO) ///Need to play around w this later
             {
                 float consequentTime = 0;
@@ -96,31 +80,47 @@ public class sGameStatistics
             return (_currentRoundEndtime - _currentRoundTimeStart) / cycles;
         }
 
-        Debug.Log($"no Cycles for station ");
+        //Debug.Log($"!!..no Cycles for station ");
         return (_currentRoundEndtime - _currentRoundTimeStart);
     }
+    /// <summary>
+    /// Total # of kits not yet shipped
+    /// TED: we do not count individual parts as WIP, but rather the amount of kits in the simulation
+    /// </summary>
     public int GetWIP()
     {
-        ///TED: we do not count individual parts as WIP, but rather the amount of kits
-        /// in the simulation
-
-        ///OLD and wrong:
-        //var gm = GameManager.instance;
-        //int batchSize = gm._batchSize; ///If we change batch sizes mid game, move Batch inside CreatedOrder wrapper
-        //var config = gm.AssemblyBook;
-        //int totalWip = 0;
-        //foreach (var order in _orders)
-        //{
-        //    totalWip += config.GetRequiredComponentsForPart(order.ItemId).Count * batchSize;
-        //}
-
-
         ///TODO - In the future, real LEAN says
         ///WIP doesnt start till kitting pushes first batch
         ///or shipping pulls an item 
-        
-        ///For now just return the # of orders
+
+        ///For now just return the # of orders since this matches the # of kits 
         return _orders.Count;
+    }
+    public void AddedADefect(int stationID, int itemID) { ++Defects; }
+    public void CreatedAnOrder(int itemID, float createdTime, float expectedTime)
+    {
+        ItemOrder order = new ItemOrder(itemID, createdTime, expectedTime);
+        _orders.Enqueue(order);
+    }
+    public void StationSentBatch(int stationID, int batchSize, bool wasShipped, float time)
+    {
+       //Debug.Log($"{stationID}::<color=White> Batch Sent Somewhere </color> = {wasShipped}  --> ORDERCOUNT= {_orders.Count}");
+        if (wasShipped)
+            ShippedAnOrder(time);
+
+        ///Keep track of cycletime
+        if (_cycleTimes.TryGetValue(stationID, out Queue<float> times))
+        {
+            times.Enqueue(time);
+            return;
+        }
+
+        ///Creates a new Entry
+        Queue<float> newTimesQueue = new Queue<float>();
+        //newTimesQueue.Enqueue(_timeGameStarted); //perhaps not?
+        newTimesQueue.Enqueue(time);
+        _cycleTimes.Add(stationID, newTimesQueue);
+
     }
 
     /************************************************************************************************************************/
@@ -133,8 +133,10 @@ public class sGameStatistics
 
         ItemOrder fifoOrder = _orders.Dequeue();
         float timeToShipThisItem = currTime - fifoOrder.StartTime;
+        if(timeToShipThisItem < 0)
+            Debug.Log($"<color=red>negative...timeToShipThisItem = </color> {timeToShipThisItem} from CurrTime:{currTime} - {fifoOrder.StartTime}");
         totalShippingTime += timeToShipThisItem;
-        Debug.Log($"...PromisedTime = {fifoOrder.PromisedTime} vs CurrTime = {currTime}");
+        //Debug.Log($"...PromisedTime = {fifoOrder.PromisedTime} vs CurrTime = {currTime}");
         if (currTime > fifoOrder.PromisedTime )
         {
             ++ShippedLate;
