808d1011e2bf5550df7fc4c6c471e4a6b3f60361
Steve Datz <sfdatz@live.com>
2021-06-21 10:08:07 -0500

    comments

--- Assets/Scripts/World/Objects/PartBin.cs
+++ Assets/Scripts/World/Objects/PartBin.cs
@@ -8,11 +8,8 @@ public class PartBin : HighlightableObject
     [SerializeField] string _animTxt = "CompAdded";
     [SerializeField] BatchEvent _uiEvent = default;
 
-
-
     private ClickToShow _toggle;
     private bool _isSelected;
-
 
 
     protected override void Awake()
@@ -23,15 +20,11 @@ public class PartBin : HighlightableObject
             _uiEvent.OnEventRaised += PlayUIAnim;
     }
 
-
     private void Follow(Vector3 toLoc)
     {
-
         Vector3 onlyHoriz = new Vector3(toLoc.x, transform.position.y, toLoc.z);
         transform.position = Vector3.Lerp(transform.position, onlyHoriz, 0.5f);
-
     }
-
 
 
     public override void OnInteract()
@@ -46,19 +39,20 @@ public class PartBin : HighlightableObject
         }
     }
 
+    /// <summary>
+    /// Used to trigger an animation when items get added/removed from the bins via network
+    /// Instead of adding a new void event for IN ( added 1 component at a time) and listening for 2 event types,
+    /// decided to make the in-event a batchEvent full of dummy data. Will probably move to the network sending individual items
+    /// as batches at some point soon 
+    /// </summary>
+    /// <param name="dummyData"></param>
     private void PlayUIAnim(BatchWrapper dummyData)
     {
-        Debug.Log($"Play anim");
         if(_animator)
         {
             _animator.SetTrigger(_animTxt);
         }
     }
-
-
-
-
-
 
 
 }
